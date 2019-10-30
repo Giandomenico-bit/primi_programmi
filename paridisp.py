2792a636bbbbe3481b9c396bfd60c11c4fef537c
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-10-30 22:22:16 +0100

    paridisp.py

--- paridisp.py
+++ paridisp.py
@@ -1,11 +1,9 @@
 # Numero pari o dispari
 
 value = input( "Enter a value : ")
-intValue = int( value ) #cast esplicito
+value = int( value ) #cast esplicito
 
-if value != floatValue :
-    print( "Il numero non appartiene a Z")
-elif value & 1 :
+if value & 1 :
     print( "Il numero e' dispari" )
 else :
-    print( "Il numero e' pari ")
+    print( "Il nuemro e' pari" )
