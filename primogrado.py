fe661a8efe8f81395a343b380a366e0cbf90c8e7
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-10-30 01:33:27 +0100

    Risoluzione equazione primo grado

---
+++ primogrado.py
@@ -0,0 +1,17 @@
+# calcolo soluzioni equazione primo grado
+
+print( "Equazione ax + b = 0" )
+
+a = input( "Inserisci un valore per a : ")
+print( a,"x + b = 0" )
+b = input( "Inserisci un valore per b : ")
+print( a,"x +",b,"= 0" )
+
+a = float( a )
+b = float( b )
+
+if a == 0 :
+    print( "Equazione impossibile" )
+else :
+    x = -( b / a ) 
+    print( "Il valore di x e' : ", x )
