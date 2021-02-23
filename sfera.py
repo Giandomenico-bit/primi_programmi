028b684089c79c1497111a497795522d48cad0ce
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-02-23 16:50:14 +0100

    Update

--- sfera.py
+++ sfera.py
@@ -1,13 +1,18 @@
-#Calcolo superficie e volume
-#di una sfera
+# Creare un programma sfera.py che stampi
+# superficie e volume di una sfera dato il raggio (intero).
+
+# Per portare a termine il compito è
+# necessaria la costante pi greco. Si può
+# utilizzare il valore già definito nella
+# libreria matematica: import math
 
 import math
 
-radius = input( "Enter radius : ")
-radius = int( radius )   #cast esplicito
+raggio = input( "Inserisci raggio : ")
+raggio = int( raggio )   # cast esplicito
 
-area = 4.0 * math.pi * radius * radius
-volume = 4.0/3.0 * math.pi * radius * radius * radius
+area = 4.0 * math.pi * float( raggio**2 )
+volume = 4.0 / 3.0 * math.pi * float( raggio**3 )
 
-print( "Area is : ", area )
-print( "Volume is : ", volume )
+print( "Area : ", area )
+print( "Volume : ", volume )
