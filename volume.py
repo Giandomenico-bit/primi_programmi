21f81392dcb32231990714e07eda92c8443149f4
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-02-23 17:00:15 +0100

    Update

--- volume.py
+++ volume.py
@@ -1,28 +1,33 @@
-# implementazione del costrutto
-# selettivo if-else
+# Si scriva un programma che calcoli il volume
+# di un cubo o di una sfera in base ad una
+# scelta effettuata dall'utente. Se l'utente sceglie di
+# calcolare il volume di un cubo, il programma
+# chiederà l'input del relativo lato,
+# altrimenti chiederà il raggio della sfera.
 
 import math
 
 print( "Calcolo del volume di un cubo o di una sfera" )
 
 print( "1 - cubo, 2 - sfera" )
-choice = input( "? " )
+scelta = input( "? " )
 
-choice = int( choice ) #cast esplicito
-if choice == 1 :
-    side = input( "Lato del cubo : ")
-    side = float( side )
-    
-    volume = side * side * side
-    print( "Il cubo di lato", side, "ha volume", volume )
+scelta = int( scelta ) # cast esplicito
 
-elif choice == 2 :
-    radius = input( "Raggio della sfera : ")
+if scelta == 1 :
+    lato = input( "Lato del cubo : ")
+    lato = int( lato )
 
-    radius = float( radius ) #cast esplicito
-    volume = 4.0/3.0 * math.pi * radius * radius * radius
+    volume = lato**3
+    print( "Il cubo di lato", lato, "ha volume", volume )
 
-    print( "La sfera di raggio", radius, "ha volume", volume )
+elif scelta == 2 :
+    raggio = input( "Raggio della sfera : ")
+
+    raggio = int( raggio ) #cast esplicito
+    volume = 4.0 / 3.0 * math.pi * raggio**3
+
+    print( "La sfera di raggio", raggio, "ha volume", volume )
 
 else :
     print( "Numero non valido" )
